7877f35ea8c8030b4570378369683d7a44960257
Jackie Elliott <t-jaelli@microsoft.com>
2018-06-08 17:17:12 -0700

    Added connection string for email information and authentication

--- email/email.go
+++ email/email.go
@@ -3,7 +3,9 @@ package email
 import (
 	"fmt"
 	"io/ioutil"
+	"net/url"
 	"os"
+	"strconv"
 
 	"github.com/Azure/spec-sla-bot/template"
 	gomail "gopkg.in/gomail.v2"
@@ -15,20 +17,31 @@ func SendEmailToAssignee() error {
 	b, err := ioutil.ReadFile("finalTemplate.html")
 	if err != nil {
 		fmt.Print(err)
+		return err
 	}
 	str := string(b) // convert content to a 'string'
 	//fmt.Print(str)
 	m := gomail.NewMessage()
-	m.SetHeader("From", "t-jaelli@microsoft.com")
+	//Get connection string from azure
+	emailUrl := os.Getenv("CUSTOMCONNSTR_EMAIL_URL")
+
+	//parse connection string url
+	parsed, err := url.Parse(emailUrl)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return err
+	}
+	port, _ := strconv.Atoi(parsed.Port())
+	user := parsed.User.Username()
+	password, _ := parsed.User.Password()
+	email := user + "@" + parsed.Hostname()
+	m.SetHeader("From", email)
 	m.SetHeader("To", "t-jaelli@microsoft.com")
 	m.SetHeader("Subject", "TEST")
 	m.SetBody("text/html", str)
 
-	user := os.Getenv("USERNAME")
-	password := os.Getenv("PASSWORD")
-
 	//Send the email
-	d := gomail.NewDialer("smtp.office365.com", 587, user, password)
+	d := gomail.NewDialer("smtp.office365.com", port, email, password)
 	if err = d.DialAndSend(m); err != nil {
 		return err
 	}
